e14b36a1850d85a0de8a54092acc07ff49494968
diptangsu <deep.dippu@gmail.com>
2018-06-10 18:31:11 +0530

    added blog page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_blogs)
+    path('', views.all_blogs, name='allblogs'),
+    path('<int:blog_id>/', views.blog, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 
 def all_blogs(request):
     allblogs = Blog.objects
     return render(request, 'blog/allblogs.html', {'blogs': allblogs})
+
+
+def blog(request, blog_id):
+    blogobject = get_object_or_404(Blog, id=blog_id)
+    return render(request, 'blog/blog.html', {'blog': blogobject})
